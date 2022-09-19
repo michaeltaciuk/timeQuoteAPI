14e72113d1b5a316392aca65f49196783d55fbf1
Michael Taciuk <taciuk73@gmail.com>
2022-09-19 18:17:40 -0400

    Added test quotes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const axios = require("axios")
 const bodyParser = require("body-parser")
 
+import quotes from "quotes.json"
+
 const PORT = process.env.PORT || 5000
 const app = express()
 
@@ -9,29 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-  res.send("Welcome to your App!")
-})
-
-app.get("/users", (req, res) => {
-  axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(function(response) {
-      res.json(response.data)
-    }).catch(function(error) {
-      res.json("Error occured!")
-  })
-})
-
-app.post("/getUserById", (req, res) => {
-  if (!req.body.id) {
-    res.json("No ID found in reqest body.")
-  } else {
-    axios.get(`https://jsonplaceholder.typicode.com/users/${req.body.id}`)
-      .then(function(response) {
-        res.json(response.data)
-      }).catch(function(error) {
-        res.json("Error occured!")
-      })
-  }
+  const id = Math.floor(Math.random() * quotes.length)
+  res.send(quotes)
 })
 
 app.listen(PORT, function () {
